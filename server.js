e688b3a8ec79a96410f71b62d701e091c0032411
Dinh Thuc Nguyen <thuc_nd@septeni-technology.jp>
2018-12-25 15:44:14 +0700

    add eslint for project

--- server.js
+++ server.js
@@ -1,29 +1,28 @@
-const express = require('express');
-const server = express();
-const fs = require('fs');
-const path = require('path');
-//obtain bundle
-const bundle =  require('./dist/server.bundle.js');
-//get renderer from vue server renderer
+const express = require('express')
+const server = express()
+const fs = require('fs')
+const path = require('path')
+//  obtain bundle
+const bundle = require('./dist/server.bundle.js')
+//  get renderer from vue server renderer
 const renderer = require('vue-server-renderer').createRenderer({
-  //set template
+  //  set template
   template: fs.readFileSync('./index.html', 'utf-8')
-});
+})
 
-server.use('/dist', express.static(path.join(__dirname, './dist')));
+server.use('/dist', express.static(path.join(__dirname, './dist')))
 
-//start server
+//  start server
 server.get('*', (req, res) => {
-
   bundle.default({ url: req.url }).then((app) => {
-    //context to use as data source
-    //in the template for interpolation
+    //  context to use as data source
+    //  in the template for interpolation
     const context = {
       title: 'Vue JS - Server Render',
       meta: `
         <meta description="vuejs server side render">
       `
-    };
+    }
 
     renderer.renderToString(app, context, function (err, html) {
       if (err) {
@@ -35,10 +34,10 @@ server.get('*', (req, res) => {
       } else {
         res.end(html)
       }
-    });
+    })
   }, (err) => {
-    console.log(err);
-  });
-});
+    console.log(err.message)
+  })
+})
 
-server.listen(8080);
+server.listen(8080)
